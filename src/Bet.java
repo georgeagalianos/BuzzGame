0f6b5f88436fbe23bd2330121bf796a1f9c142c3
george agalianos <58746551+georgeagalianos@users.noreply.github.com>
2020-11-06 19:28:07 +0200

    asd

--- src/Bet.java
+++ src/Bet.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class Bet {
+    Scanner console = new Scanner(System.in);
+    private int coins=0;
+    private Player player1;
+    public void BET(Player player1,){
+        this.player1=player1;
+        boolean flag=false;
+        do{
+            System.out.print("Enter the coins of the bet:");
+            coins= console.nextInt();
+            if(coins!=250 || coins!=500 || coins!=750 || coins!=1000)
+            {
+                System.out.println("DEN YPARXEI TETOI POSO NA PONTARETE");
+            }
+            else{
+                flag=true;
+            }
+        }while(flag==true);
+
+    }
+
+    public void ANSWER(boolean flag){
+        if(flag==true){
+            player1.addPoints(coins);
+        }
+        else {
+            player1.addPoints(-coins);
+        }
+    }
+
+
 }
